18dde8652c00dda02025eaa1012a4e049629eca1
Fabio Guedes dos Santos <fabiowave@gmail.com>
2021-04-28 16:46:45 -0300

    'inital'

---
+++ API/Controllers/LoggerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoggerController : ControllerBase
+    {
+
+        private readonly ILogger<LoggerController> _logger;
+
+        public LoggerController(ILogger<LoggerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("log-info")]
+        public IActionResult LogInfo()
+        {
+            _logger.LogInformation("Log Criado");
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("log-error")]
+        public IActionResult LogError()
+        {
+            _logger.LogError("Log Criado");
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("log-warning")]
+        public IActionResult LogWarning()
+        {
+            _logger.LogWarning("Log Criado");
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("log-exception")]
+        public IActionResult LogException()
+        {
+            throw new Exception("Erro exemplo");          
+        }
+
+
+    }
+}
---
+++ API/Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            //ELASTIC
+            var elasticUri = Configuration["ElasticConfiguration:Uri"];
+            var indexPatern = Configuration["ElasticConfiguration:IndexPatern"];
+
+            Log.Logger = new LoggerConfiguration()
+               .Enrich.FromLogContext()
+               .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUri))
+               {
+                   AutoRegisterTemplate = true,
+                   IndexFormat = indexPatern
+               })
+           .CreateLogger();
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            //SWAGGER
+            services.AddSwaggerGen();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            //SWAGGER
+            app.UseSwagger();
+            //SWAGGER
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //SERILOG
+            loggerFactory.AddSerilog();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
